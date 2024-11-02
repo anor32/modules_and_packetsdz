24f75d54b90b477845a5497204467469297bb757
anor23 <f@>
2024-11-02 22:48:07 +0300

    реструкторизация кода исправление ошибки

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 from sys import path
-path.append(r"..\modules_and_packetsdz\my_modules")
-path.append(r"..\modules_and_packetsdz\packets\folder1")
-path.append(r"..\modules_and_packetsdz\packets\folder2")
-path.append(r"..\modules_and_packetsdz\packets\folder2\folder3")
+path.append(r"my_modules")
+path.append(r"packets\folder1")
+path.append(r"packets\folder2")
+path.append(r"packets\folder2\folder3")
 from module_1 import path_module1
 from module_2 import path_module2
 from module_3 import path_module3
