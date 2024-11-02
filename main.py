28eba98609328e910dcdf5bf1525db7448c8f9fe
anor23 <f@>
2024-11-02 22:38:59 +0300

    добавление новых путей к папкам

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
 from sys import path
 path.append(r"..\modules_and_packetsdz\my_modules")
-
-
+path.append(r"..\modules_and_packetsdz\packets\folder1")
+path.append(r"..\modules_and_packetsdz\packets\folder2")
+path.append(r"..\modules_and_packetsdz\packets\folder2\folder3")
 from module_1 import path_module1
 
 path_module1()
@@ -14,3 +15,5 @@ path.append(r"..\modules_and_packetsdz\my_modules")
 from module_2 import path_module2
 
 path_module2()
+
+
