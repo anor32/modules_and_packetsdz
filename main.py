b007d414d34b3f83becb839980b7557230a07dbf
anor23 <f@>
2024-11-02 22:42:10 +0300

    импортирование 8 модулей и вызов функции

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ path.append(r"..\modules_and_packetsdz\packets\folder1")
 path.append(r"..\modules_and_packetsdz\packets\folder2")
 path.append(r"..\modules_and_packetsdz\packets\folder2\folder3")
 from module_1 import path_module1
-
-path_module1()
-
-
-
-path.append(r"..\modules_and_packetsdz\my_modules")
-
 from module_2 import path_module2
-
+from module_3 import path_module3
+from module_4 import path_module4
+from module_5 import path_module5
+from module_6 import path_module6
+from module_7 import path_module7
+from module_8 import path_module8
+path_module1()
 path_module2()
-
-
+path_module3()
+path_module4()
+path_module5()
+path_module6()
+path_module7()
+path_module8()
