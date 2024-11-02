1e7258e5c16893e209289d26370f5390c81e471b
anor23 <f@>
2024-11-02 22:34:50 +0300

    импорт функции второго модуля

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 
 from sys import path
 path.append(r"..\modules_and_packetsdz\my_modules")
-print(path)
+
 
 from module_1 import path_module1
 
 path_module1()
+
+
+
+path.append(r"..\modules_and_packetsdz\my_modules")
+
+from module_2 import path_module2
+
+path_module2()
